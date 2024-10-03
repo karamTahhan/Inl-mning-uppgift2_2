8b226d4b059336182e424495e8f95cb0ff4a729e
karamTahhan <kata07001@utb.vaxjo.se>
2024-10-03 13:51:46 +0200

    Add project files.

---
+++ Inlämning uppgift2_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift2_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur långt har Elin hoppat?");
+            int X=int.Parse(Console.ReadLine());
+            Console.WriteLine("hur långt har Alma hoppat?");
+            int Y=int.Parse(Console.ReadLine());
+            Console.WriteLine("skillnade mellan Eling och Alma är..");
+            int sum = X - Y;
+            Console.WriteLine(sum);
+                
+
+
+
+        }
+
+
+    }
+
+}
